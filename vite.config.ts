6dfe6bab283f0471b04f43bba8ad6d1a9e9d88c2
Guillaume Turcotte <leguille.ftw@gmail.com>
2023-07-26 14:26:29 -0400

    added way to override backend url via env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,35 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: "../backend/static",
-        emptyOutDir: true,
-        sourcemap: true,
-        rollupOptions: {
-            output: {
-                manualChunks: id => {
-                    if (id.includes("@fluentui/react-icons")) {
-                        return "fluentui-icons";
-                    } else if (id.includes("@fluentui/react")) {
-                        return "fluentui-react";
-                    } else if (id.includes("node_modules")) {
-                        return "vendor";
+export default defineConfig(({command, mode}) => {
+
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        plugins: [react()],
+        build: {
+            outDir: "../backend/static",
+            emptyOutDir: true,
+            sourcemap: true,
+            rollupOptions: {
+                output: {
+                    manualChunks: id => {
+                        if (id.includes("@fluentui/react-icons")) {
+                            return "fluentui-icons";
+                        } else if (id.includes("@fluentui/react")) {
+                            return "fluentui-react";
+                        } else if (id.includes("node_modules")) {
+                            return "vendor";
+                        }
                     }
                 }
             }
-        }
-    },
-    server: {
-        proxy: {
-            "/query": "http://localhost:5000",
-            "/chat": "http://localhost:5000"
+        },
+        server: {
+            proxy: {
+                "/query": env.VITE_API_BACKEND
+            }
         }
     }
 });
