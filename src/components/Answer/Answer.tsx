90f388892b141ee1e517375e532c33b2631715f0
Guillaume Turcotte <leguille.ftw@gmail.com>
2023-08-30 12:56:49 -0400

    initial rework of all the icons in 1 menu

--- src/components/Answer/Answer.tsx
+++ src/components/Answer/Answer.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { DirectionalHint, ITooltipProps, Stack, TooltipDelay, TooltipHost } from "@fluentui/react";
+import { ITooltipProps, Stack } from "@fluentui/react";
 import DOMPurify from "dompurify";
 
 import styles from "./Answer.module.css";
@@ -11,8 +11,9 @@ import { AnswerIcon } from "./AnswerIcon";
 import { useTranslation } from "react-i18next";
 
 import { IIconProps } from '@fluentui/react';
-import { ActionButton, IconButton } from '@fluentui/react/lib/Button';
-import { useId } from '@fluentui/react-hooks';
+import { IconButton } from '@fluentui/react/lib/Button';
+
+import { useBoolean } from '@fluentui/react-hooks';
 
 interface Props {
     answer: AskResponse;
@@ -24,7 +25,7 @@ interface Props {
     showFollowupQuestions?: boolean;
 }
 
-const sourceIcon: IIconProps = { iconName: 'Source' };
+const sourceIcon: IIconProps = { iconName: 'Source'};
 const like: IIconProps = { iconName: 'Like' };
 const dislike: IIconProps = { iconName: 'Dislike' };
 const copy: IIconProps = { iconName: 'Copy'}
@@ -45,78 +46,57 @@ export const Answer = ({
 
     const { t } = useTranslation();
 
-    // Use useId() to ensure that the ID is unique on the page.
-    // (It's also okay to use a plain string and manually ensure uniqueness.)
-    const tooltipId = useId('tooltip');
-
-    const tooltipProps: ITooltipProps = {
-        onRenderContent: () => (
-            <div>
-                <IconButton iconProps={like} />
-                <IconButton iconProps={dislike} /> 
-                <IconButton iconProps={copy} />
-            </div>
-        ),
-    };
+    const [toggleMenu, {toggle: toggleMenuVisiblity }] = useBoolean(false);
 
     return (
-        <TooltipHost id={tooltipId} tooltipProps={tooltipProps} directionalHint={DirectionalHint.rightTopEdge} closeDelay={3000} delay={TooltipDelay.medium}>
-            <Stack className={`${styles.answerContainer} ${isSelected && styles.selected}`} verticalAlign="space-between">
-                <Stack.Item>
-                    <Stack horizontal horizontalAlign="space-between" aria-describedby={tooltipId}>
-                        <AnswerIcon />
-                        <div className={styles.sourcesContainer}>
-                            {/* <IconButton
-                                style={{ color: "black" }}
-                                iconProps={{ iconName: "Lightbulb" }}
-                                title="Show thought process"
-                                ariaLabel="Show thought process"
-                                onClick={() => onThoughtProcessClicked()}
-                                disabled={!answer.thoughts}
-                            /> */}
+        <Stack className={`${styles.answerContainer} ${isSelected && styles.selected}`} verticalAlign="space-between" onMouseEnter={() => toggleMenuVisiblity()} onMouseLeave={() =>  toggleMenuVisiblity()}>
+            <Stack.Item>
+                <Stack horizontal horizontalAlign="space-between">
+                    <AnswerIcon />
+                    {toggleMenu ?
+                    (<div className={styles.sourcesContainer}>
+                        <IconButton iconProps={like} className={styles.menuIcons} />
+                        <IconButton iconProps={dislike} className={styles.menuIcons} /> 
+                        <IconButton iconProps={copy} onClick={() => navigator.clipboard.writeText(sanitizedAnswerHtmlPre)} className={styles.menuIcons} />
+                        <IconButton iconProps={sourceIcon} allowDisabledFocus disabled={!answer.metadata} onClick={() => onSupportingContentClicked()} title={t("supporting")} ariaLabel={t("supporting")} className={styles.menuIcons} />
+                    </div>) : null}
+                </Stack>
+            </Stack.Item>
 
-                            <ActionButton iconProps={sourceIcon} allowDisabledFocus disabled={!answer.metadata} onClick={() => onSupportingContentClicked()} title={t("supporting")} ariaLabel={t("supporting")}>
-                            Source(s)
-                            </ActionButton>
-                        </div>
+            <Stack.Item grow>
+                <div className={styles.answerText} dangerouslySetInnerHTML={{ __html: sanitizedAnswerHtml }}></div>
+            </Stack.Item>
+
+            {!!parsedAnswer.citations.length && (
+                <Stack.Item>
+                    <Stack horizontal wrap tokens={{ childrenGap: 5 }}>
+                        <span className={styles.citationLearnMore}>Citations:</span>
+                        {parsedAnswer.citations.map((x, i) => {
+                            const path = getCitationFilePath(x);
+                            return (
+                                <a key={i} className={styles.citation} title={x} onClick={() => onCitationClicked(path)}>
+                                    {`${++i}. ${x}`}
+                                </a>
+                            );
+                        })}
                     </Stack>
                 </Stack.Item>
+            )}
 
-                <Stack.Item grow>
-                    <div className={styles.answerText} dangerouslySetInnerHTML={{ __html: sanitizedAnswerHtml }}></div>
+            {!!parsedAnswer.followupQuestions.length && showFollowupQuestions && onFollowupQuestionClicked && (
+                <Stack.Item>
+                    <Stack horizontal wrap className={`${!!parsedAnswer.citations.length ? styles.followupQuestionsList : ""}`} tokens={{ childrenGap: 6 }}>
+                        <span className={styles.followupQuestionLearnMore}>Follow-up questions:</span>
+                        {parsedAnswer.followupQuestions.map((x, i) => {
+                            return (
+                                <a key={i} className={styles.followupQuestion} title={x} onClick={() => onFollowupQuestionClicked(x)}>
+                                    {`${x}`}
+                                </a>
+                            );
+                        })}
+                    </Stack>
                 </Stack.Item>
-
-                {!!parsedAnswer.citations.length && (
-                    <Stack.Item>
-                        <Stack horizontal wrap tokens={{ childrenGap: 5 }}>
-                            <span className={styles.citationLearnMore}>Citations:</span>
-                            {parsedAnswer.citations.map((x, i) => {
-                                const path = getCitationFilePath(x);
-                                return (
-                                    <a key={i} className={styles.citation} title={x} onClick={() => onCitationClicked(path)}>
-                                        {`${++i}. ${x}`}
-                                    </a>
-                                );
-                            })}
-                        </Stack>
-                    </Stack.Item>
-                )}
-
-                {!!parsedAnswer.followupQuestions.length && showFollowupQuestions && onFollowupQuestionClicked && (
-                    <Stack.Item>
-                        <Stack horizontal wrap className={`${!!parsedAnswer.citations.length ? styles.followupQuestionsList : ""}`} tokens={{ childrenGap: 6 }}>
-                            <span className={styles.followupQuestionLearnMore}>Follow-up questions:</span>
-                            {parsedAnswer.followupQuestions.map((x, i) => {
-                                return (
-                                    <a key={i} className={styles.followupQuestion} title={x} onClick={() => onFollowupQuestionClicked(x)}>
-                                        {`${x}`}
-                                    </a>
-                                );
-                            })}
-                        </Stack>
-                    </Stack.Item>
-                )}
-            </Stack>
-        </TooltipHost>
+            )}
+        </Stack>
     );
 };
