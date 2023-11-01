c3abc2410dc4a206323c11a1b98d9395fd63f8b9
Guillaume Turcotte <leguille.ftw@gmail.com>
2023-11-01 08:53:17 -0400

    added code to support new title metadata and backward compatibility o… (#40)
    * added code to support new title metadata and backward compatibility of older indexes
    * removed gpt35 from the options

--- src/components/SupportingContent/SupportingContent.tsx
+++ src/components/SupportingContent/SupportingContent.tsx
@@ -13,10 +13,13 @@ export const SupportingContent = ({ supportingContent }: Props) => {
             {Object.keys(supportingContent).map((key) => (
                 supportingContent[key].text.map((t, i) => (
                     <li className={styles.supportingContentItem}>
-                        {supportingContent[key].url != "" ? 
-                            (<h4 className={styles.supportingContentItemHeader}><a href={supportingContent[key].url} target="_blank">{supportingContent[key].url}</a></h4>) : 
+                        {supportingContent[key].title && supportingContent[key].url &&
+                            (<h4 className={styles.supportingContentItemHeader}><a href={supportingContent[key].url} target="_blank">{supportingContent[key].title}</a></h4>)}
+                        {!supportingContent[key].title && supportingContent[key].url &&
+                            (<h4 className={styles.supportingContentItemHeader}><a href={supportingContent[key].url} target="_blank">{supportingContent[key].url}</a></h4>)}
+                        {!supportingContent[key].title && !supportingContent[key].url &&
                             (<h4 className={styles.supportingContentItemHeader}>{supportingContent[key].filename}</h4>)}
-                        {/*<h5>{supportingContent[key].node_scores[i]*100}</h5>*/}
+                        {/* <h5>{supportingContent[key].node_scores[i]*100}</h5> */}
                         <p className={styles.supportingContentItemText}>{DOMPurify.sanitize(parseAnswerToHtml(t, function(){}).answerHtml)}</p>
                     </li>
                 ))
